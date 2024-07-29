7d25c441f14a7a74fe432faea43bf22282a0c0a4
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2024-07-29 17:23:23 +0000

    --fix: corrigindo eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,16 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-     <Head>
-       
+      <Head>
         <link rel="icon" href="/SIGSport.svg" />
         <title>SigSports</title>
-        <meta name="description" content="Plataforma de gerenciamento de esportes" />
-        
+        <meta
+          name="description"
+          content="Plataforma de gerenciamento de esportes"
+        />
       </Head>
 
       <body className="h-screen w-screen bg-bgGray">
