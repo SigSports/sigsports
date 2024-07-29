693a92a93a00f78fa5feb0d0d205a70b4608db3a
Valtercio Santiago Ferreira <juniorvaltercio2000@gmail.com>
2024-07-29 14:16:10 -0300

    --fix: Update title and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     <Head>
+       
+        <link rel="icon" href="/SIGSport.svg" />
+        <title>SigSports</title>
+        <meta name="description" content="Plataforma de gerenciamento de esportes" />
+        
+      </Head>
 
       <body className="h-screen w-screen bg-bgGray">
         <Main />
